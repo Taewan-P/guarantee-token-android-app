1802cb3e4d62026e438974c17d22d7fdca9c84c1
Taewan Park <swimingtw@gmail.com>
2022-03-04 18:28:13 +0900

    Add account info in the edittext

--- app/src/main/java/dev/chungjungsoo/guaranteewallet/adapter/TokenListViewAdapter.kt
+++ app/src/main/java/dev/chungjungsoo/guaranteewallet/adapter/TokenListViewAdapter.kt
@@ -7,15 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.BaseAdapter
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dev.chungjungsoo.guaranteewallet.R
 import dev.chungjungsoo.guaranteewallet.dataclass.ListViewItem
+import dev.chungjungsoo.guaranteewallet.preference.PreferenceUtil
 
 
 class TokenListViewAdapter(private val items: MutableList<ListViewItem>) : BaseAdapter() {
+    companion object {
+        lateinit var prefs: PreferenceUtil
+    }
     private val colorList = listOf(
         R.drawable.shape_listiview_tokens,
         R.drawable.shape_listiview_tokens_color2,
@@ -38,10 +43,11 @@ class TokenListViewAdapter(private val items: MutableList<ListViewItem>) : BaseA
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        prefs = PreferenceUtil(parent!!.context)
         var listView = convertView
 
         if (listView == null) {
-            listView = LayoutInflater.from(parent?.context)
+            listView = LayoutInflater.from(parent.context)
                 .inflate(R.layout.listview_item_list_token, parent, false)
         }
 
@@ -56,9 +62,12 @@ class TokenListViewAdapter(private val items: MutableList<ListViewItem>) : BaseA
         listView.setBackgroundResource(colorList[position % colorList.size])
 
         listView.findViewById<ImageView>(R.id.send_token_btn).setOnClickListener {
-            val bottomSheetDialog = BottomSheetDialog(parent!!.context)
+            val bottomSheetDialog = BottomSheetDialog(parent.context)
             val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_send_sheet, parent, false)
-            
+
+            view.findViewById<EditText>(R.id.send_from_input).setText(prefs.getString("account", ""))
+
+
             bottomSheetDialog.setCancelable(true)
             bottomSheetDialog.setContentView(view)
             bottomSheetDialog.dismissWithAnimation = true
